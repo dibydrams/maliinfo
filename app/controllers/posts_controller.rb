6231a5dc9fe28dde47d3f257b681f28eef470fba
Diby Drams <djibril.drame@live.fr>
2015-02-16 02:21:40 +0100

    JS fixed, Thumbnails fixed, Add article page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,10 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	private
 
   def set_available_data
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,11 +3,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   
   version :standard do
-    process :resize_to_fill => [100, 150, :north]
+    process :resize_to_fill => [630, 420, :north]
   end
   
   version :thumbnail do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [300, 200]
   end     
     
 end
