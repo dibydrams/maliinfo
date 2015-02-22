344229f9728204fb05fe84deeeca2c394c10f3ef
Diby Drams <djibril.drame@live.fr>
2015-02-22 09:16:42 +0100

    Change social icons + Carrousel + responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require responsivecarrousel.min.js
 
-$(document).ready(function() {
-  $("#carrousel_controls").on('click', 'span', function() {
-    $("#carrousel img").removeClass("opaque");
+// $(document).ready(function() {
+//   $("#carrousel_controls").on('click', 'span', function() {
+//     $("#carrousel img").removeClass("opaque");
 
-    var newImage = $(this).index();
+//     var newImage = $(this).index();
 
-    $("#carrousel img").eq(newImage).addClass("opaque");
+//     $("#carrousel img").eq(newImage).addClass("opaque");
 
-    $("#carrousel_controls span").removeClass("selected");
-    $(this).addClass("selected");
-  });
-});
+//     $("#carrousel_controls span").removeClass("selected");
+//     $(this).addClass("selected");
+//   });
+// });
