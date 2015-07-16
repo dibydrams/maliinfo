8c43e232791eeab21d79553849eca5ad3b7c2039
Diby Drams <djibril.drame@live.fr>
2015-07-16 03:51:57 +0200

    Fix LinkedIn, change post route, add date

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   root 'posts#index'
-  resources :posts
+  resources :posts, path: ''
   resources :categories, only: [:show]
 end
